05a571fa616ebbb19f7502220929211ed2927d8c
Kotova Dana <44918208+kotovadana@users.noreply.github.com>
2019-06-01 12:56:09 +0300

    deleted security + templates

---
+++ src/main/java/kotova/sda/entity/RegistrationList.java
@@ -0,0 +1,21 @@
+package kotova.sda.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class RegistrationList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String userName;
+    private String gender;
+    private String email;
+    private String password;
+
+
+
+}
--- src/main/java/kotova/sda/entity/ToDoList.java
+++ src/main/java/kotova/sda/entity/ToDoList.java
@@ -27,4 +27,78 @@ public class ToDoList {
     private  Date dayX;
     private String status;
     //
+
+    public  ToDoList () {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Date getPlannedEndDate() {
+        return plannedEndDate;
+    }
+
+    public void setPlannedEndDate(Date plannedEndDate) {
+        this.plannedEndDate = plannedEndDate;
+    }
+
+    public Date getDayX() {
+        return dayX;
+    }
+
+    public void setDayX(Date dayX) {
+        this.dayX = dayX;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
