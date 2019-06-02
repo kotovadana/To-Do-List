3c6bcbe2d82debfd8251914f4713bce9dbdb2a9f
Kotova Dana <44918208+kotovadana@users.noreply.github.com>
2019-06-02 12:36:15 +0300

    finished with form and mapping

--- src/main/java/kotova/sda/controllers/MainController.java
+++ src/main/java/kotova/sda/controllers/MainController.java
@@ -2,6 +2,8 @@ package kotova.sda.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,8 +15,21 @@ public class MainController {
         return "firstpage_form";
     }
 
-    @RequestMapping ("/PersonalToDoList/Registration")
-    String gav() {
+    //@RequestMapping ("/PersonalToDoList/Registration")
+    //String gav() {return "registration_form";}
+
+    @GetMapping("/PersonalToDoList/List")
+    private String list() {
+        return "todo_list";
+    }
+
+    @GetMapping("/PersonalToDoList/Registration")
+    private String registrate() {
         return "registration_form";
     }
+    @GetMapping("/PersonalToDoList/Registration/Successful_reg")
+    private String successreg() {
+        return "successreg_form";
+    }
+
 }
